daf3942b1768c3022074d8587128b61ed933f48c
kylelconrad <kylelconrad@gmail.com>
2017-06-13 15:48:20 -0600

    Add files via upload

---
+++ try1.js
@@ -0,0 +1,16 @@
+var night = document.getElementById("night");
+night.addEventListener("click", setNightMode);
+
+
+function setNightMode() {
+console.log("night");
+document.getElementById("night").textContent;
+document.getElementById("night").textContent = "Day Mode"; document.getElementById("wrapper").style.backgroundImage = "url('night.jpg)";
+console.log(document.getElementById("wrapper").style.backgroundImage);
+console.log(document.getElementById("wrapper").textContent);
+document.getElementById("poem").textContent = "the darkness of the sky overwhelms";
+}
+function onclick {
+    
+}
+    
