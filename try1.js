7df0804d19ff3d4d8d6e5b827d0f1446bda97c77
kylelconrad <kylelconrad@gmail.com>
2017-06-16 14:15:17 -0600

    Add files via upload

--- try1.js
+++ try1.js
@@ -4,7 +4,7 @@ night.addEventListener("click", setNightMode);
 
 function setNightMode() {
 console.log("night");
-document.getElementById("night").textContent;
+//document.getElementById("night").textContent;
 document.getElementById("night").textContent = "Day Mode"; document.getElementById("wrapper").style.backgroundImage = "url('night.jpg)";
 console.log(document.getElementById("wrapper").style.backgroundImage);
 console.log(document.getElementById("wrapper").textContent);
